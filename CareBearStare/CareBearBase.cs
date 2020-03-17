1d5a219c2925fe45427b1d686a2ec05beb09bc62
Jacob <wittenberg.jacob@gmail.com>
2020-03-17 18:41:35 -0500

    Add project files.

---
+++ CareBearStare/CareBearBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareBearStare
+{
+    class CareBearBase
+    {
+        public string Name { get; set; }
+        public BearColor Color { get; set; }
+        public string BellyBadge { get; set; }
+
+        public abstract void Stare();
+
+        public void Care(string humanToCareFor)
+        {
+            Console.WriteLine($"{Name} cares deeply for {humanToCareFor}.");
+        }
+
+        public virtual void Hug(CareBearBase careBearToHug)
+        {
+            Console.WriteLine($"{Name} runs over to {careBearToHug.Name} and squeezes them, pressing their {BellyBadge} and {careBearToHug.BellyBadge} together.");
+        }
+    }
+
+    enum BearColor
+    {
+        Yellow,
+        Pink,
+        White,
+        Aqua,
+        Mauve,
+        Purple,
+        Gold,
+        Blue,
+        Brown
+    }
+}
---
+++ CareBearStare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CareBearStare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
